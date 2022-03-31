a9a75dad6ca252a2dafa2ed2a6cef2e77c8d3eb4
김찬주 <kt30019196@gmail.com>
2022-03-31 12:02:11 +0900

    sql커밋

--- LunchBoxSQL.sql
+++ LunchBoxSQL.sql
@@ -26,42 +26,61 @@ create table restaurant_info(
     n_star int(100)
 );
 create table menu_info(
-	email varchar(100),
-    pw varchar(100),
-    nick varchar(100),
-    Join_date date
+	menu_id int(100),
+    res_id int(100),
+    menu_name varchar(100),
+    category_b int(100),
+    category_s int(100),
+    kw varchar(100)
 );
 create table review_info(
+	re_id int(100),
 	email varchar(100),
-    pw varchar(100),
-    nick varchar(100),
-    Join_date date
+    menu_id int(100),
+    eval int(100),
+    t_score int(100),
+    m_score int(100),
+    c_score int(100),
+    re_date timestamp,
+    content varchar(100),
+    cook_time int(100),
+    wait_time int(100),
+    rec int(100),
+    pic int(100)
 );
 create table PIC_info(
-	email varchar(100),
-    pw varchar(100),
-    nick varchar(100),
-    Join_date date
+	pic_id int(100),
+    rev_id int(100),
+	p_file varchar(100),
+    img_size int(100),
+    img_loc varchar(100),
+    img_type varchar(100),
+    org_n varchar(100)
 );
 create table BLOG_info(
-	email varchar(100),
-    pw varchar(100),
-    nick varchar(100),
-    Join_date date
+	blog_id int(100),
+    res_id int(100),
+    url varchar(100),
+    b_date date,
+    ad_nonad int(100),
+    con_t varchar(100),
+    con_b varchar(100),
+    word_fr varchar(100)
+    
 );
 #데이터입력
-insert into nodejs_member values('1','1','1');
+insert into user_info values('1','1','1');
 
 #데이터검색
-select * from nodejs_member;
-select * from message_member;
+select * from user_info;
+select * from review_info;
 #데이터수정
 # edit > preferances > sql editor > 맨마지막에 체크(safe update) 풀고 다시시작하기
-update nodejs_member 
+update user_info 
 set pw = '1234'
-where id = '1'; 
+where email = '1'; 
 #데이터삭제
-delete from nodejs_member where id = '1'
+delete from user_info where email = '1'
 
 select * from sessions
 
